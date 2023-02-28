db963cfddab7f4c78c425b7157c91d9773adb13f
harrywangsw <68875077+harrywangsw@users.noreply.github.com>
2023-02-27 20:11:08 -0500

    Update save_load.cs

--- Assets/scripts/data/save_load.cs
+++ Assets/scripts/data/save_load.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-
-public class save_load : MonoBehaviour
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+using System;
+public static class save_load 
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void SavePlayer(stats stattobesaved, string player_name)
     {
-        
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = (string)Application.persistentDataPath+"/"+player_name+".pl";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        stats stat = stattobesaved;
+        formatter.Serialize(stream, stat);
+        stream.Close();
+        Debug.Log("saved!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public static stats LoadPlayer(string loadname)
     {
-        
+        string path = Application.persistentDataPath + "/" + loadname +".pl";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            stats data = formatter.Deserialize(stream) as stats;
+            stream.Close();
+            Debug.Log("loaded");
+            return data;
+        }
+        else
+        {
+            Debug.Log("path not found");
+            return null;
+        }
     }
 }
